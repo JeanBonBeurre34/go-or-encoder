809b0ae593a5ad08840d7f1159201a9fd5c0adc0
Reg0l <129545703+JeanBonBeurre34@users.noreply.github.com>
2025-10-18 17:41:27 +0200

    Create main.go
    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "os"
+)
+
+func main() {
+        key := byte(0x9A)
+
+        if len(os.Args) < 2 {
+                fmt.Println("Usage: encrypt <path>")
+                os.Exit(1)
+        }
+
+        // Get the path from the command line argument
+        path := os.Args[1]
+
+        _, err := os.Stat(path)
+        if err != nil {
+                log.Fatalf("[FATAL] File %s doesn't exist", path)
+        }
+        fmt.Println("[+] Reading bytes of shellcode")
+        // Read the entire file into a byte slice
+        fileData, err := os.ReadFile(path)
+        if err != nil {
+                log.Fatalf("[FATAL] Error reading file:", err)
+        }
+
+        // Encrypt data and add to encryptedData 
+        encryptedData := make([]byte, len(fileData))
+        fmt.Println("[+] Encrypting bytes of shellcode")
+
+        for i := 0; i < len(encryptedData); i++ {
+                encryptedData[i] = fileData[i] ^ key
+        }
+        fmt.Printf("[+] Writing encoded bytes at %s", path+"_ENC")
+
+        // Write encryptedData to file
+        os.WriteFile(path+"_ENC", encryptedData, 0644)
+}
